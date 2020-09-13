48f50747e297765f95787707cccb969f4b1dbe70
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-09-13 13:36:03 -0600

    Add error messages and data vadilation

--- main.js
+++ main.js
@@ -11,13 +11,16 @@ var accomplishInput = document.querySelector("#accomplish");
 var buttonWrap = document.querySelector(".category-buttons");
 var hideForm = document.querySelector(".new-activity");
 var activityTimer = document.querySelector(".current-activity-box")
-var startButton = document.querySelector(".start-button")
-var categorySelected;
+var startButton = document.querySelector(".start-button");
+var inputFields = document.querySelectorAll("input");
+var accomplishError = document.querySelector(".accomplish-input-error");
+var minutesError = document.querySelector(".minutes-input-error");
+var secondsError = document.querySelector(".seconds-input-error");
+var categorySelected = "";
 var savedActivities = [];
 
-
-activityButton.addEventListener('click', removeForm);
-activityButton.addEventListener('click', newInstance);
+    
+activityButton.addEventListener('click', dataValidate);
 buttonWrap.addEventListener("click", buttonState);
 buttonWrap.addEventListener("click", setCategory);
 
@@ -57,6 +60,7 @@ function setCategory(event) {
   if (event.target.className.includes("study-select") || event.target.id === "study") {
     categorySelected = "Study"
     startButton.classList.add("start-button-green")
+    console.log("test3");
   } else if (event.target.className.includes("meditate-select") || event.target.id === "meditate") {
     categorySelected = "Meditate"
     startButton.classList.add("start-button-purple")
@@ -79,6 +83,45 @@ function newInstance() {
 }
 
 function removeForm() {
+  debugger
   hideForm.classList.add("hidden");
   activityTimer.classList.remove("hidden");
+}   
+
+
+  
+function dataValidate() {
+  console.log("test");
+  activityButton.disabled = false;
+  console.log(activityButton.disabled);
+  if (categorySelected && accomplishInput.value.length > 0 && minutesInput.value.length > 0 && secondsInput.value.length > 0) {
+    // activityButton.disabled = false;
+    console.log("form valid")
+    removeForm();
+    newInstance();
+  } else {
+    // activityButton.disabled = true;
+    for( var i = 0; i < inputFields.length; i++) {
+      if (inputFields[0].value.length === 0) {
+        console.log("test");
+        accomplishError.classList.remove("hidden");
+      }
+      if (inputFields[1].value.length === 0) {
+        minutesError.classList.remove("hidden");
+      }
+      if(inputFields[2].value.length === 0) {
+        secondsError.classList.remove("hidden");
+      }
+    }  
+    if(!categorySelected) {
+      console.log("error")
+    }
+  }
+  // for( var i = 0; i < inputFields.length; i++) {
+  //   if (inputFields[i].value.length == 0) {
+  //     activityButton.disabled = true;
+  //   } else if (inputFields[0].value.length > 0 && inputFields[1].value.length > 0 && inputFields[2].value.length > 0) {
+  //     activityButton.disabled = false;
+  
 }
+
