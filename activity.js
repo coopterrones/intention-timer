fc7bf5f326bed51b6ccaadd883247109222d7ab5
Ben Prat <bvprat@gmail.com>
2020-09-15 16:47:29 -0600

    Add activity card functionality. Add acticity card to completed side.

--- activity.js
+++ activity.js
@@ -1,9 +1,10 @@
 class Activity {
-  constructor(category, description, minutes, seconds, completed, id) {
+  constructor(category, description, minutes, seconds, color, completed, id) {
     this.category = category;
     this.description = description;
     this.minutes = minutes;
     this.seconds = seconds;
+    this.color = color;
     this.completed = false;
     this.id = id;
   }
